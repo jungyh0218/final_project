7ff06ae7bff4220084db4ffbd37bb8d0ee3ca36d
jungyh0218 <jungyh0218@gmail.com>
2015-12-11 16:26:09 +0900

    first commit

---
+++ app/src/androidTest/java/knu/mobile/lookatmyenglish/ApplicationTest.java
@@ -0,0 +1,13 @@
+package knu.mobile.lookatmyenglish;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
